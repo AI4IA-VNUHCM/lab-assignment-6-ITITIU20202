523b8f04c4c396e2f6aa2d4ec5c3ca487fde2ee6
ITITIU20202 <ititiu20202@student.hcmiu.edu.vn>
2021-11-22 18:35:57 +0700

    update ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -15,7 +15,50 @@ Ex:
 
 void Ex3(char *str){
 	//Your codes here
-	
+	int max = 0,min=strlen(str);
+	int count =0;
+	int start=0;
+	char temp[100],temp1[100];
+	for(int i =0;i<strlen(str);i++) {
+	    
+			if (str[i]==' ') {
+				if (count > max) {
+
+					max = count;
+					for (int t =0; t <max;t++) {
+						temp[t]=str[start++];
+					}
+					temp[max] ='\0';
+				}
+				count =0;
+				start = i+1;
+				continue;
+			}
+			count++;
+		}
+	printf("Longest word: %s\n",temp);
+
+	for(int i =0;i<strlen(str);i++) {
+	    
+			if (str[i]==' ') {
+				if (count < min) {
+
+					min = count;
+					for (int t =0; t <count;t++) {
+						temp1[t]=str[start++];
+					}
+					temp1[count] ='\0';
+				}
+				count =0;
+				start = i+1;
+				continue;
+			}
+			count++;
+		}
+	printf("Shortest word: %s",temp1);
+
+
+
 }
 
 int main(int argc, char *argv[]) {
