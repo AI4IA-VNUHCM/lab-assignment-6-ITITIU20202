083354535fdc2e795b6c93921816f9fa3c3c61ef
ITITIU20202 <ititiu20202@student.hcmiu.edu.vn>
2021-11-22 17:45:46 +0700

    update ex5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -17,7 +17,33 @@ Ex:
 
 void Ex5(char *str){
 	//Your codes here
-	
+	int size = strlen(str);
+	printf("size la: %d\n", size);
+	if (size%2!=0) printf("Invalid!");
+	else {
+		switch (size) {
+
+			case 6:
+			if (str[0]=='{' && str[5]=='}' && str[1]=='[' && str[4]==']' && str[2]=='(' && str[3]== ')') {
+				printf("Valid!");
+				} else printf("Invalid!");
+				break;
+		
+
+			case 4: 
+				if (str[0]=='{' && str[3]=='}' && str[1]=='[' && str[2]==']') { 
+					printf ("Valid!");
+				} else printf("Invalid");
+				break;
+
+			case 2:
+				if ((str[0]=='{' && str[1]=='}') || (str[0]=='[' && str[1]==']') || (str[0]=='(' && str[1]==')')) {
+					printf("Valid!"); 
+					} else printf("Invalid!");
+				break;
+			
+	}
+	}
 }
 
 int main(int argc, char *argv[]) {
